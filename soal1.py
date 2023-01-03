77f8b20e2da49ee4a70d6ebddef8b5cba057208a
agnesiayolanda <117900996+agnesiayolandaaa@users.noreply.github.com>
2023-01-03 14:55:12 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,104 @@
+print ("==========================================")
+print ("************* Justice League *************")
+print ("==========================================")
+username = input("Masukkan username anda:")
+username1= ("brucewyne")
+username2= ("victorstone")
+username3= ("ciscoramon")
+
+if (username1):
+    print("====== WELCOME brucewyne=====")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus anggota ")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+    pilihan= int(input("Masukkan pilihan anda:"))
+elif (username2):
+    print("====== WELCOME victorstone=====")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus anggota ")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+    pilihan= int(input("Masukkan pilihan anda:"))
+elif (username3):
+    print("====== WELCOME ciscoramon=====")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus anggota ")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+    pilihan= int(input("Masukkan pilihan anda:"))
+elif (username4):
+    print ("Access Denied")
+else:
+    exit()
+
+pilihan1 = ("1. Tambah Anggota Justice League")
+pilihan2 = ("2. Hapus anggota ")
+pilihan3 = ("3. Tampilkan Anggota Justice League")
+pilihan4 = ("4. Exit")
+
+anggotabaru= input("Nama Anggota baru:")
+
+anggotabaru1= ("Barry Allen")
+anggotabaru2= ("Diana Prince")
+anggotabaru3= ("Arthur Curry")
+
+if (anggotabaru):
+    print ("data 'Barry Allen' berhasil ditambahkan")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus anggota ")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+elif (anggotabaru2):
+    print ("data 'Diana Prince' berhasil ditambahkan")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus anggota ")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+elif (anggotabaru3):
+    print ("data 'Arthur Curry' berhasil ditambahkan")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus anggota ")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+else:
+    exit()
+
+pilihan= int(input("Masukkan pilihan anda:"))
+dihapus= input("Anggota yang akan dihapus:")
+
+if (anggotabaru1):
+    print ("data 'Barry Allen' berhasil dihapus")
+elif (anggotabaru2):
+    print ("data 'Diana Prince' berhasil dihapus")
+elif (anggotabaru3):
+    print ("data 'Arthur Curry' berhasil dihapus")
+else:
+    print ("data 'Slade Wilson' tidak ditemukan")
+
+pilihan= int(input("Masukkan pilihan anda:"))
+
+if (pilihan3):
+    print ("| Barry Allen || Diana Prince|| Arthur Curry| ")
+    print("1. Tambah Anggota Justice League")
+    print("2. Hapus anggota ")
+    print("3. Tampilkan Anggota Justice League")
+    print("4. Exit")
+else:
+    exit()
+
+pilihan= int(input("Masukkan pilihan anda:"))
+if (anggotabaru1):
+    print ("see u next time MR. bruceywne, GLHF")
+elif (anggotabaru2):
+    print ("see u next time MR. victorstone, GLHF")
+elif (anggotabaru3):
+     print ("see u next time MR. ciscoramon, GLHF")
+else:
+    exit()
+
+
+
+
+
+
